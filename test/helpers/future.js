b791f93b99a302fb2515885dd13f9c025ac9c217
TimLuq <timluq+gitcommit@gmail.com>
2018-12-20 15:36:42 +0100

    fix to run tests in node 8

--- test/helpers/future.js
+++ test/helpers/future.js
@@ -1,57 +1,22 @@
 function future() {
-    /**
-     * @type {[boolean, any] | undefined | null}
-     */
-    let v;
-    let complete;
-    let completeExceptionally;
+    const o = {};
     const prom = new Promise(function (resolve, reject) {
-        if (v) {
-            prom.complete = function complete(_) { return false; };
-            prom.completeExceptionally = function completeExceptionally(_) { return false; };
-            if (v[0]) {
-                resolve(v[1]);
-            } else {
-                reject(v[1]);
-            }
-        } else {
-            complete = function complete(a) {
-                if (v) { return false; }
-                else { v = [true, a]; resolve(a); return true; }
-            };
-            completeExceptionally = function complete(a) {
-                if (v) { return false; }
-                else { v = [false, a]; reject(a); return true; }
-            };
-        }
+        o.complete = function complete(a) {
+            if (o.v) { return false; }
+            else { o.v = [true, a]; resolve(a); return true; }
+        };
+        o.completeExceptionally = function completeExceptionally(a) {
+            if (o.v) { return false; }
+            else { o.v = [false, a]; reject(a); return true; }
+        };
     });
-    prom.complete = function (val) {
-        if (v) {
-            return false;
-        } else {
-            v = [true, val];
-            if (complete) {
-                return complete(val);
-            }
-            return true;
-        }
-    };
-    prom.completeExceptionally = function (val) {
-        if (v) {
-            return false;
-        } else {
-            v = [false, val];
-            if (completeExceptionally) {
-                return completeExceptionally(val);
-            }
-            return true;
-        }
-    };
+    prom.complete = function complete(val) { return o.complete(val); };
+    prom.completeExceptionally = function completeExceptionally(val) { return o.completeExceptionally(val); };
     prom.asCallback = function futureAsCallback(err, val) {
         if (err) {
-            prom.completeExceptionally(err);
+            o.completeExceptionally(err);
         } else {
-            prom.complete(val);
+            o.complete(val);
         }
     }
     return prom;
